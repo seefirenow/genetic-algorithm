2b8c350962483c75b807ddeb924792313d19b007
Julia Napora <jula.napa.111115@gmail.com>
2022-05-03 19:44:29 +0200

    2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,18 @@ function App(props) {
   const [enteredN, setEnteredN] = useState("10");
   const [enteredT, setEnteredT] = useState("100");
   const [data, setData] = useState([]);
+  const [dataPi, setDatPi] = useState([]);
+  const [checkedMax, setCheckedMax] = React.useState(true);
+  const [checkedMin, setCheckedMin] = React.useState(false);
 
+  const handleChangeOne = () => {
+    setCheckedMax(!checkedMax);
+    setCheckedMin(!checkedMin);
+  };
+  const handleChangeTwo = () => {
+    setCheckedMin(!checkedMin);
+    setCheckedMax(!checkedMax);
+  };
   const aChangeHandler = (event) => {
     setEnteredA(event.target.value);
   };
@@ -54,9 +65,22 @@ function App(props) {
 
     return xbin;
   }
-  function wart(a, b, d) {
-    const dataTable = [];
 
+  function lp(n) {
+    const arrayLp = [];
+    if (data.length > n - 1) {
+      data.length = 0;
+    }
+    for (let i = 1; i <= n; i++) {
+      arrayLp[i - 1] = i;
+    }
+    return arrayLp;
+  }
+  function xreal(a, b, d) {
+    if (dataPi.length > n - 1) {
+      dataPi.length = 0;
+    }
+    const arrayX = [];
     a = Math.ceil(a);
     b = Math.floor(b);
 
@@ -67,47 +91,137 @@ function App(props) {
 
     let prec = d.length - 2;
     d = parseFloat(d);
-    if (dataTable.length > n - 1) {
-      dataTable.length = 0;
+    if (arrayX.length > n - 1) {
+      arrayX.length = 0;
     }
-    for (let i = 1; i <= n; i++) {
+
+    for (let i = 0; i < n; i++) {
       const h = Math.random() * (b - a);
       const xR = h + a;
       const xxR = parseFloat(xR);
       const xReal = xxR.toFixed(prec);
-      const xInt = parseInt(realToInt(xReal));
-      const xBin = intToBin(xInt);
-      const xI = parseInt(binToInt(xBin));
-      const xRr = intToreal(xI) + a;
-      const xRrr = parseFloat(xRr.toFixed(prec));
-      const f = (xRrr % 1) * (Math.cos(20 * Math.PI * xRrr) - Math.sin(xRrr));
-
-      dataTable.push({
-        lp: i,
-        xreal: xReal,
-        xint: xInt,
-        xbin: xBin,
-        xi: xI,
-        xrrr: xRrr,
-        f: f,
-      });
+      arrayX.push(xReal);
     }
-    console.log(dataTable);
-    return dataTable;
+    return arrayX;
   }
-  const dataChangeHandler = (event) => {
-    const dataTable = wart(a, b, d);
-    setData(dataTable);
+
+  function fun(xReal) {
+    const dataFun = [];
+
+    for (let i = 1; i <= xReal.length; i++) {
+      const x = parseFloat(xReal[i - 1]);
+      const funkcja = (x % 1) * (Math.cos(20 * Math.PI * x) - Math.sin(x));
+      dataFun.push(funkcja);
+    }
+
+    return dataFun;
+  }
+  function gun(min, max, f, d) {
+    d = parseFloat(d);
+    const arrayG = [];
+    if (checkedMax === true) {
+      for (let i = 1; i <= f.length; i++) {
+        const g = f[i - 1] - min + d;
+        arrayG.push(g);
+      }
+      return arrayG;
+    } else {
+      for (let i = 1; i <= f.length; i++) {
+        const g = f[i - 1] - max - d;
+        arrayG.push(g);
+      }
+      return arrayG;
+    }
+  }
+  function gSum(g) {
+    let gSum = 0;
+    for (let i = 0; i < g.length; i++) {
+      gSum += g[i];
+    }
+    return gSum;
+  }
+  function pii(g, sumG) {
+    const arrayPI = [];
+    for (let i = 0; i < g.length; i++) {
+      const pi = g[i] / sumG;
+      arrayPI.push(pi);
+    }
+    return arrayPI;
+  }
+
+  function qu(pi) {
+    const arrayQu = [];
+    let qi = 0;
+    for (var i = 0; i < pi.length; i++) {
+      if (i === 0) {
+        qi = pi[i];
+        arrayQu.push(qi);
+      } else {
+        qi = arrayQu[i - 1] + pi[i];
+        arrayQu.push(qi);
+      }
+    }
+
+    return arrayQu;
+  }
+  function randR() {
+    const arrayR = [];
+    for (let i = 0; i < n; i++) {
+      const rR = Math.random() * (1 - 0) + 0;
+      const r = parseFloat(rR);
+      arrayR.push(r);
+    }
+    return arrayR;
+  }
+
+  function xrealtwo(n, q, r, x) {
+    const arrayxtwo = [];
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n; j++) {
+        if (q[j] >= r[i]) {
+          arrayxtwo[i] = parseFloat(x[j]);
+          break;
+        }
+      }
+    }
+    return arrayxtwo;
+  }
+
+  const dataHandler = (event) => {
+    const tab = [];
+    const lpi = lp(n);
+    const x = xreal(a, b, d, n);
+    const f = fun(x);
+    const min = Math.min.apply(Math, f);
+    const max = Math.max.apply(Math, f);
+    const g = gun(min, max, f, d);
+    const sumaG = gSum(g);
+    const pi = pii(g, sumaG);
+    const q = qu(pi);
+    const r = randR();
+    const xreltwo = xrealtwo(n, q, r, x);
+    const ftwo = fun(xreltwo);
+    for (let i = 0; i < n; i++) {
+      tab[i] = {
+        lp: lpi[i],
+        x: x[i],
+        f: f[i],
+        g: g[i],
+        pi: pi[i],
+        q: q[i],
+        r: r[i],
+        xtwo: xreltwo[i],
+        ftwo: ftwo[i],
+      };
+    }
+    setData(tab);
+    console.log(tab);
   };
 
   const submmitHandler = (event) => {
     event.preventDefault();
-
-    const formValue = {
-      data: wart(a, b, d),
-    };
-    console.log(formValue);
   };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -152,7 +266,30 @@ function App(props) {
               value={enteredT}
               onChange={tChangeHandler}
             ></input>
-            <button type="submit" onClick={dataChangeHandler}>
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  value={checkedMax}
+                  checked={checkedMax}
+                  onChange={handleChangeOne}
+                />
+                MAX
+              </label>
+            </div>
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  value={checkedMin}
+                  checked={checkedMin}
+                  onChange={handleChangeTwo}
+                />
+                MIN
+              </label>
+            </div>
+
+            <button type="submit" onClick={dataHandler}>
               start
             </button>
           </form>
@@ -163,11 +300,13 @@ function App(props) {
               <tr>
                 <th>Lp</th>
                 <th>xReal</th>
-                <th>xInt</th>
-                <th>xBin</th>
-                <th>xInt</th>
-                <th>xReal</th>
                 <th>f(x)</th>
+                <th>g(x)</th>
+                <th>p(i)</th>
+                <th>q</th>
+                <th>r</th>
+                <th>xReal*</th>
+                <th>f(x)*</th>
               </tr>
             </thead>
             <tbody>
@@ -175,12 +314,14 @@ function App(props) {
                 return (
                   <tr key={key}>
                     <td>{val.lp}</td>
-                    <td>{val.xreal}</td>
-                    <td>{val.xint}</td>
-                    <td>{val.xbin}</td>
-                    <td>{val.xi}</td>
-                    <td>{val.xrrr}</td>
+                    <td>{val.x}</td>
                     <td>{val.f}</td>
+                    <td>{val.g}</td>
+                    <td>{val.pi}</td>
+                    <td>{val.q}</td>
+                    <td>{val.r}</td>
+                    <td>{val.xtwo}</td>
+                    <td>{val.ftwo}</td>
                   </tr>
                 );
               })}
